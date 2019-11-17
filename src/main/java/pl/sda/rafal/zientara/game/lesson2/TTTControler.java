5ad4650b873f1659ef4f3dd99cba2ffd40a69ba9
Sailor Jerry <michalmycek@protonmail.com>
2019-11-17 14:37:28 +0100

    JavaFX_TTTGame1

---
+++ src/main/java/pl/sda/rafal/zientara/game/lesson2/TTTControler.java
@@ -0,0 +1,27 @@
+package pl.sda.rafal.zientara.game.lesson2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TTTControler {
+
+
+    @FXML
+    private Button actionTopLeft;
+    @FXML
+    private Button actionTopCenter;
+    @FXML
+    private Button actionTopRight;
+    @FXML
+    private Button actionCenterLeft;
+    @FXML
+    private Button actionCenter;
+    @FXML
+    private Button actionCenterRight;
+    @FXML
+    private Button actionBottomCenter;
+    @FXML
+    private Button actionBottomLeft;
+    @FXML
+    private Button actionBottomRight;
+}
