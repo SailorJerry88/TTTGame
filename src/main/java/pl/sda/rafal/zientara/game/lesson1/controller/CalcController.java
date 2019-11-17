8d91d564cbf939e7b13cb8ed2351d23b7246c571
Sailor Jerry <michalmycek@protonmail.com>
2019-11-17 14:00:05 +0100

    JavaFX_adding_buttonsWTF

--- src/main/java/pl/sda/rafal/zientara/game/lesson1/controller/CalcController.java
+++ src/main/java/pl/sda/rafal/zientara/game/lesson1/controller/CalcController.java
@@ -3,39 +3,39 @@ package pl.sda.rafal.zientara.game.lesson1.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
+
+import java.math.BigDecimal;
 
 public class CalcController {
 
 
     @FXML
     private TextField pole;
-    private int value;
+    private BigDecimal value;
     private Operation currentOperation;
+    private boolean shouldClearOnAppend = false;
 
-    @FXML
-    private void handlePlusPressed(ActionEvent event) {
+    private BigDecimal getValueFromScreen() {
+        return new BigDecimal(pole.getText());
+    }
+
+    private void handleOperationPressed(Operation operation){
         value = Integer.parseInt(pole.getText());
-        currentOperation = Operation.ADD;
+        currentOperation = operation;
         pole.clear();
     }
 
-    private int getValueFromScreen(){
-        return Integer.parseInt(pole.getText());
-    }
-
-
-    @FXML
-    private void handleEqualsPressed() {
-        int secondValue = getValueFromScreen();
-        int output = calculate(secondValue);
-    }
-
-    private int calculate(int secondValue){
+    private int calculate(BigDecimal secondValue){
 
         switch (currentOperation){
             case ADD:
                 return value + secondValue;
+            case MINUS:
+                return value - secondValue;
+            case MULT:
+                return value / secondValue;
+            case DIV:
+                return value * secondValue;
 
         }
         return 0;
@@ -44,67 +44,107 @@ public class CalcController {
 
 
     @FXML
+    private void handlePlusPressed(ActionEvent event) {
+        handleOperationPressed(Operation.ADD);
+    }
+    @FXML
+    private void handleMinusPressed(ActionEvent event) {
+      handleOperationPressed(Operation.MINUS);
+    }
+    @FXML
+    private void handleMultPressed(ActionEvent event) {
+       handleOperationPressed(Operation.MULT);
+    }
+    @FXML
+    private void handleDivPressed(ActionEvent event) {
+        handleOperationPressed(Operation.DIV);
+    }
+    @FXML
+    private void handleEqualsPressed() {
+        BigDecimal secondValue = getValueFromScreen();
+        BigDecimal output = calculate(secondValue);
+        String text = String.valueOf(output);
+        addToScreen(text);
+        shouldClearOnAppend = true;
+    }
+
+    @FXML
+    private void handleACPressed() {
+        pole.clear();
+        value = new BigDecimal(0);
+        currentOperation = null;
+        addToScreen("0");
+    }
+
+    @FXML
     private void handleZeroPressed(ActionEvent event) {
         System.out.println("0");
-        pole.appendText("0");
+        addToScreen("0");
     }
 
     @FXML
     private void handle1Pressed(ActionEvent event) {
         System.out.println("1");
-        pole.appendText("1");
+        addToScreen("1");
     }
 
     @FXML
     private void handle2Pressed(ActionEvent event) {
         System.out.println("2");
-        pole.appendText("2");
+        addToScreen("2");
     }
 
     @FXML
     private void handle3Pressed(ActionEvent event) {
         System.out.println("3");
-        pole.appendText("3");
+        addToScreen("3");
     }
 
     @FXML
     private void handle4Pressed(ActionEvent event) {
         System.out.println("4");
-        pole.appendText("4");
+        addToScreen("4");
     }
 
     @FXML
     private void handle5Pressed(ActionEvent event) {
         System.out.println("5");
-        pole.appendText("5");
+        addToScreen("5");
     }
 
     @FXML
     private void handle6Pressed(ActionEvent event) {
         System.out.println("6");
-        pole.appendText("6");
+        addToScreen("6");
     }
 
     @FXML
     private void handle7Pressed(ActionEvent event) {
         System.out.println("7");
-        pole.appendText("7");
+        addToScreen("7");
     }
 
     @FXML
     private void handle8Pressed(ActionEvent event) {
         System.out.println("8");
-        pole.appendText("8");
+        addToScreen("8");
     }
 
     @FXML
     private void handle9Pressed(ActionEvent event) {
         System.out.println("9");
-        pole.appendText("9");
+        addToScreen("9");
     }
 
-
+    private void addToScreen(String appendText) {
+        pole.appendText(appendText);
+    }
 }
+
+
+
+
+
 
 
 
